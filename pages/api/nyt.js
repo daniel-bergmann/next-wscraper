f52a782c473321d46b60accc846c0ffaeb161e9c
Daniel Bergmann <dba222@outlook.com>
2021-10-07 21:15:22 +0000

    first commit

---
+++ pages/api/nyt.js
@@ -0,0 +1,28 @@
+const puppeteer = require('puppeteer');
+// // const cors = require('cors');
+// // const app = express();
+// // app.use(cors());
+
+export default async function handler(req, res) {
+  async function scrape() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const url = 'https://www.nytimes.com/section/world';
+
+    await page.goto(url);
+
+    // looking for combined elements
+    const nytOutput = await page.evaluate(() =>
+      Array.from(document.querySelectorAll('article')).map((d) => ({
+        title: d.querySelector('h2, css-byk1jx').innerText.trim(),
+        pic: d.querySelector('img').src,
+        link: d.querySelector('a').href,
+      }))
+    );
+
+    await browser.close();
+    res.send({ nytOutput });
+  }
+  // calling function
+  scrape();
+}
---
+++ pages/index.js
@@ -0,0 +1,82 @@
+
+
+import { server } from '../config';
+import styled from 'styled-components';
+
+export default function Home({ data }) {
+  return (
+    <div>
+      {data.nytOutput.map((i) => {
+        return (
+          <ArticleStyle key={i.title}>
+            <a href={i.link}>
+              <img src={i.pic} alt='' />
+              <h2>{i.title}</h2>
+            </a>
+          </ArticleStyle>
+        );
+      })}
+    </div>
+  );
+}
+
+const ArticleStyle = styled.article`
+ 
+    display: grid;
+    grid-template-columns: 1fr;
+    border-radius: 10px;
+    margin: 1rem;
+    max-width: 800px;
+    a {
+      color: inherit;
+      text-decoration: none;
+      h2 {
+        padding: 5px;
+      }
+      img {
+        width: 100%;
+        border-radius: 10px 10px 0px 0px;
+      }
+    }
+  }
+  // for large screens
+  @media (min-width: 768px) {
+ 
+      margin: 1rem auto;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      &:hover {
+        background: rgb(243, 243, 243);
+        transition: ease-in 0.2s;
+        img {
+          transition: ease-in 0.2s;
+          opacity: 0.9;
+        }
+      }
+      a {
+        display: flex;
+        img {
+          width: 400px;
+          border-radius: 10px 0px 0px 10px;
+        }
+        h2 {
+          width: 300px;
+        }
+      }
+    
+  
+`;
+
+export async function getServerSideProps() {
+  const res = await fetch(`${server}/api/nyt`);
+  const data = await res.json();
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
+}
